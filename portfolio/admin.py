57caed6bcebfb5af2b057d92bd75107c8cd5a221
RoBruns <rodrigobrunsfh@gmail.com>
2024-01-10 19:22:52 -0300

    Fazendo o menu de navegação

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from portfolio import models
+
+
 # Register your models here.
+@admin.register(models.Projects)
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'technology', 'conclusion_date')
+    list_filter = ('technology', 'conclusion_date')
+    search_fields = ('id', 'title', 'technology', 'conclusion_date')
+    date_hierarchy = 'conclusion_date'
+    ordering = ('conclusion_date',)
+    readonly_fields = ('conclusion_date',)
+    list_per_page = 10
--- portfolio/models.py
+++ portfolio/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class Projects(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
-    technology = models.CharField(max_length=100)
+    description = models.TextField(("Description"), blank=True, null=True)
+    technology = models.CharField(max_length=100, null=True)
     image = models.ImageField(upload_to='portfolio/images/', blank=True, null=True) # noqa 
     url = models.URLField(blank=True, null=True)
     github_link = models.URLField(blank=True)
